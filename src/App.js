0bd3b2c005cf386bd7c1b3c445e72baf6d24e2a9
Omar Cabatbat <omar_cabatbat@yahoo.com>
2023-11-21 13:19:20 -0500

    fetch data from API and show the name in the page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  constructor() {
+    super()
 
-    </div>
-  );
+    this.state = {
+      monsters: [],
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then((monsters) => {
+        this.setState
+          (() => {
+            return { monsters: monsters }
+          })
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {
+          this.state.monsters.map((monster) => {
+            return (
+              <div key={monster.id}>
+                <h1>{monster.name}</h1>
+              </div>)
+          })
+        }
+      </div>
+    );
+  }
+
 }
 
 export default App;
